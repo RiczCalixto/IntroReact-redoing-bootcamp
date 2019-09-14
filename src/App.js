dd8c22746baf2b082e98a3fa10a75b7a0840c4c8
Ricardo Calixto <rcalixtos@gmail.com>
2019-09-14 17:06:15 -0300

    configuração do webpack para inserir assets no arquivo .html (utilizando o file-loader)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+import male from "./assets/male.jpg";
 
 function App() {
-  return <h1>Oi do componente react</h1>;
+  return (
+    <>
+      <h1>Oi do componente react</h1>
+      <img src={male} alt="" />
+    </>
+  );
 }
 
 export default App;
