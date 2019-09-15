c296794b18cba9091056afb9e79eff1ca5f3c255
Ricardo Calixto <rcalixtos@gmail.com>
2019-09-15 17:50:49 -0300

    Inserção dos itens da lista de forma dinamica atraves do input/botão de submit.

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -2,18 +2,38 @@ import React, { Component } from "react";
 
 class TechList extends Component {
   state = {
+    newTech: "",
     techs: ["Node.js", "React.js", "React Native"]
   };
+
+  handleInputAdd = e => {
+    this.setState({ newTech: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: ""
+    });
+  };
+
   render() {
-    console.log(this.state);
     return (
-      <>
+      <form onSubmit={this.handleSubmit}>
+        <h1>{this.state.newTech}</h1>
         <ul>
-          {this.state.techs.map(s => (
-            <li>{s}</li>
+          {this.state.techs.map(tech => (
+            <li key={tech}>{tech}</li>
           ))}
         </ul>
-      </>
+        <input
+          type="text"
+          onChange={this.handleInputAdd}
+          value={this.state.newTech}
+        />
+        <button type="submit">Insira aqui!</button>
+      </form>
     );
   }
 }
