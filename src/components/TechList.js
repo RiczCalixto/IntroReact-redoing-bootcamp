208c0733753bbecdecf1bd470b2d84e400660cee
Ricardo Calixto <rcalixtos@gmail.com>
2019-09-15 19:41:53 -0300

    Aplicação do ciclo de vida dos componentes e uso do localStorage para armazenamento das lita de tecnologias(mount carrega o state com as informacoes do localStorage e o update faz a atualização do que esta armazenado no localStorage).

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -8,8 +8,21 @@ class TechList extends Component {
 
   state = {
     newTech: "",
-    techs: ["Node.js", "React.js", "React Native"]
+    techs: []
   };
+
+  componentDidMount() {
+    const techs = localStorage.getItem("techs");
+    if (techs) {
+      this.setState({ techs: JSON.parse(techs) });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.techs !== this.state.techs) {
+      localStorage.setItem("techs", JSON.stringify(this.state.techs));
+    }
+  }
 
   handleInputAdd = e => {
     this.setState({ newTech: e.target.value });
